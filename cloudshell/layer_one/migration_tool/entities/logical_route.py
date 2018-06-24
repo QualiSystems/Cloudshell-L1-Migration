73db8615e82552e721c66b2979c17ad79052e440
Yaroslav Nikonorov <yaroslav.n@quali.com>
2018-06-24 15:19:05 +0300

    Added inactive ports

--- cloudshell/layer_one/migration_tool/entities/logical_route.py
+++ cloudshell/layer_one/migration_tool/entities/logical_route.py
@@ -1,9 +1,12 @@
 class LogicalRoute(object):
-    def __init__(self, source, target, reservation_id):
+    def __init__(self, source, target, reservation_id, route_type, route_alias, active=True):
         self.source = source
         self.target = target
         self.reservation_id = reservation_id
+        self.route_type = route_type
+        self.route_alias = route_alias
         self.connections = []
+        self.active = active
 
     def __str__(self):
         return '{0}<->{1}'.format(self.source, self.target)
--- cloudshell/layer_one/migration_tool/helpers/logical_route_helper.py
+++ cloudshell/layer_one/migration_tool/helpers/logical_route_helper.py
@@ -33,17 +33,27 @@ class LogicalRouteHelper(object):
     @property
     def logical_routes_by_resource_name(self):
         if not self._logical_routes_by_resource_name:
+            active_routes = []
             for reservation in self._api.GetCurrentReservations().Reservations:
                 if reservation.Id:
                     details = self._api.GetReservationDetails(reservation.Id).ReservationDescription
                     for route_info in details.ActiveRoutesInfo:
-                        source = route_info.Source
-                        target = route_info.Target
-                        logical_route = LogicalRoute(source, target, reservation.Id)
-                        for segment in route_info.Segments:
-                            self._logical_routes_by_resource_name[segment.Source.split('/')[0]].add(logical_route)
-                            self._logical_routes_by_resource_name[segment.Target.split('/')[0]].add(logical_route)
+                        self._define_logical_route(reservation.Id, route_info, True)
+                        active_routes.append((route_info.Source, route_info.Target))
+                    for route_info in details.RequestedRoutesInfo:
+                        if (route_info.Source, route_info.Target) not in active_routes:
+                            self._define_logical_route(reservation.Id, route_info, False)
         return self._logical_routes_by_resource_name
+
+    def _define_logical_route(self, reservation_id, route_info, active=True):
+        source = route_info.Source
+        target = route_info.Target
+        route_type = route_info.RouteType
+        route_alias = route_info.Alias
+        logical_route = LogicalRoute(source, target, reservation_id, route_type, route_alias, active)
+        for segment in route_info.Segments:
+            self._logical_routes_by_resource_name[segment.Source.split('/')[0]].add(logical_route)
+            self._logical_routes_by_resource_name[segment.Target.split('/')[0]].add(logical_route)
 
     def remove_route(self, logical_route):
         """
@@ -52,7 +62,8 @@ class LogicalRouteHelper(object):
         self._logger.debug('Removing logical route {}'.format(logical_route))
         if not self._dry_run:
             self._api.RemoveRoutesFromReservation(logical_route.reservation_id,
-                                                  [logical_route.source, logical_route.target], 'bi')
+                                                  [logical_route.source, logical_route.target],
+                                                  logical_route.route_type)
 
     def create_route(self, logical_route):
         """
@@ -61,17 +72,19 @@ class LogicalRouteHelper(object):
         self._logger.debug('Creating logical route {}'.format(logical_route))
         if not self._dry_run:
             self._api.CreateRouteInReservation(logical_route.reservation_id, logical_route.source, logical_route.target,
-                                               False, 'bi', 2, '', False)
+                                               False, logical_route.route_type, 2, logical_route.route_alias, False)
+            if not logical_route.active:
+                self._api.DisconnectRoutesInReservation(logical_route.reservation_id, [logical_route.source])
 
-    def get_logical_routes_for_connection(self, connections):
-        """
-        :type connections: list
-        """
-        logical_routes = []
-        for connection in connections:
-            logical_route = self.logical_routes_by_segments.get(connection.port.name)
-            if logical_route:
-                logical_route.connections.append(connection)
-                if logical_route not in logical_routes:
-                    logical_routes.append(logical_route)
-        return logical_routes
+    # def get_logical_routes_for_connection(self, connections):
+    #     """
+    #     :type connections: list
+    #     """
+    #     logical_routes = []
+    #     for connection in connections:
+    #         logical_route = self.logical_routes_by_segments.get(connection.port.name)
+    #         if logical_route:
+    #             logical_route.connections.append(connection)
+    #             if logical_route not in logical_routes:
+    #                 logical_routes.append(logical_route)
+    #     return logical_routes
