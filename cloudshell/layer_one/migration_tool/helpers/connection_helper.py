d853a7d54fe0b42c147e76e84bd0cb0b587aa4de
Yaroslav Nikonorov <yaroslav.n@quali.com>
2018-08-31 14:10:43 +0300

    Ignore error for an operation

--- cloudshell/layer_one/migration_tool/helpers/connection_helper.py
+++ cloudshell/layer_one/migration_tool/helpers/connection_helper.py
@@ -12,7 +12,7 @@ class ConnectionHelper(object):
         """
         :type connection: cloudshell.layer_one.migration_tool.entities.connection.Connection
         """
-        self._logger.debug('Updating {}'.format(connection))
+        self._logger.debug('Updating Connection {}=>{}'.format(connection.port.name, connection.connected_to))
         if not self._dry_run:
             self._api.UpdatePhysicalConnection(connection.port.name, connection.connected_to)
             self._api.UpdateConnectionWeight(connection.port.name, connection.connected_to, connection.weight)
